197428b5884d4fbb82d40f154525f3b45cfb9a4a
Maxwell Farrell <richard.farrell2@sky.uk>
2017-05-21 12:47:45 +0100

    some tests

--- src/sheets/a-one-converter-test.js
+++ src/sheets/a-one-converter-test.js
@@ -1,7 +1,33 @@
-import xy from './a-one-converter'
+import { xy, gridRange } from './a-one-converter'
 
 describe('A1 converter', () => {
   it('converts 1, 1 to A1', () => {
     expect(xy(1, 1)).toEqual('A1')
+  })
+
+  describe('converts "GridRange"s', () => {
+    it('A1:A1', () => {
+      const input = { startRowIndex: 0, endRowIndex: 1, startColumnIndex: 0, endColumnIndex: 1 }
+
+      expect(gridRange(input)).toEqual('A1:A1')
+    })
+
+    it('A3:B4', () => {
+      const input = { startRowIndex: 2, endRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2 }
+
+      expect(gridRange(input)).toEqual('A3:B4')
+    })
+
+    it('A:B', () => {
+      const input = { startColumnIndex: 0, endColumnIndex: 2 }
+
+      expect(gridRange(input)).toEqual('A:B')
+    })
+
+    it('A5:B', () => {
+      const input = { startRowIndex: 4, startColumnIndex: 0, endColumnIndex: 2 }
+
+      expect(gridRange(input)).toEqual('A5:B')
+    })
   })
 })
--- src/sheets/a-one-converter.js
+++ src/sheets/a-one-converter.js
@@ -1,9 +1,16 @@
 const alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-const xy = (x, y) => {
+export const xy = (x, y) => {
   const col = alphabet[x - 1]
 
   return `${col}${y.toString()}`
 }
 
-export default xy
+export const gridRange = ({ startColumnIndex, startRowIndex, endColumnIndex, endRowIndex }) => {
+  const startColumn = alphabet[startColumnIndex]
+  const startRow = startRowIndex === undefined ? '' : startRowIndex + 1
+  const endColumn = alphabet[endColumnIndex - 1]
+  const endRow = endRowIndex === undefined ? '' : endRowIndex
+
+  return `${startColumn}${startRow}:${endColumn}${endRow}`
+}
