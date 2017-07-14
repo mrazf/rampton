f9a2d51306bb9d7dcd3e6ee7536cd9188b9c1b24
Maxwell Farrell <richard.farrell2@sky.uk>
2017-07-14 23:17:43 +0100

    Dunno what this is

--- src/monzo-webhooks/monzo-webhooks.js
+++ src/monzo-webhooks/monzo-webhooks.js
@@ -42,6 +42,39 @@ router.get('/monzo-webhooks', authenticate, (req, res) => {
     })
 })
 
+const newHook = ({ monzo }) => {
+  const url = `https://api.monzo.com/webhooks`
+  const headers = { Authorization: `Bearer ${monzo.token.access_token}` }
+  const form = { account_id: monzo.accountId, url: 'https://rampton.herokuapp.com/monzo-webhook' }
+
+  return new Promise((resolve, reject) => {
+    console.info(`POST ${url}`)
+
+    request.post(url, { headers, form }, (err, res, body) => {
+      if (err) {
+        console.error(`POST ${url} errored with ${err} at ${err.stack}`)
+
+        return reject(err)
+      }
+
+      const response = JSON.parse(body)
+      if (response.error) {
+        console.error(response)
+
+        return reject(response.error)
+      }
+
+      return resolve(response)
+    })
+  })
+}
+
+router.post('/monzo-webhooks', authenticate, (req, res) => {
+  configurator(req.params.uid)
+    .then(config => newHook(config))
+    .then(result => res.send(200, result))
+})
+
 router.post('/monzo-webhook', (req, res) => {
   userFromAccountId(req.body.data.account_id)
     .then(user => transformAndWrite(user, req.body.data))
